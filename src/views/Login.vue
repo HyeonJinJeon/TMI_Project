53166c7af7bba39d2005070380ee6be374bf0ff1
jeonhyeonjin <nd3807@naver.com>
2023-02-02 22:42:59 +0900

    css수정

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,27 @@
 <template>
-  <div class="black-bg">
-    <div class="white-bg">
-      <p class="h4 text-center mb-4">Sign in</p>
-      <label for="idIn" class="grey-text" >Your id</label>
-      <input type="text" id="idIn" class="form-control" v-model = "id"/>
-      <br />
-      <label for="pwIn" class="grey-text">Your password</label>
-      <input type="password" id="pwIn" class="form-control" v-model = "password"/>
-      <div class="text-center mt-4">
-        <button class="btn btn-indigo" type="submit" @click="login">Login</button>
-        <button class="btn btn-indigo" type="submit" @click="goMain">뒤로가기</button>
+  <div class="backgroundImg">
+    <div class="black-bg">
+      <div class="white-bg">
+
+        <p class="h4 text-center mb-4">Sign in</p>
+        <label for="idIn" class="grey-text" >Your id</label>
+        <input type="text" id="idIn" class="form-control" v-model = "id">
+        <br />
+        <label for="pwIn" class="grey-text">Your password</label>
+        <input type="password" id="pwIn" class="form-control" v-model = "password" v-on:keypress.enter.prevent=login>
+        <div class="text-center mt-4">
+          <button class="btn btn-indigo" type="submit" @click="login" style="color: white;">Login</button>
+          <button class="btn btn-indigo" type="submit" @click="goMain" style="color: white;">뒤로가기</button>
+        </div>
+        <!-- Default form login -->
       </div>
-      <!-- Default form login -->
     </div>
   </div>
 </template>
 
 <script>
 import {firebase} from "@/firebase/firebaseConfig";
+
 export default {
   name: 'login',
   components: {},
@@ -47,9 +51,11 @@ export default {
 </script>
 
 <style scoped>
-body {
-  background-image: url("../assets/images/bgPhoto.jpg");
+.backgroundImg {
+  background-image: url("../assets/images/startBackground.jpg");
   background-color:rgba(0, 0, 0, 0.5);
+  height: 100vh;
+  width: 100%;
   background-size: cover;
 }
 .black-bg {
